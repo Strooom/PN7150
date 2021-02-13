19888d0c54a909f1992f94deb462d6626d768e97
Strooom <pascal.roobrouck@gmail.com>
2021-02-13 18:54:02 +0100

    add empty PN7150.h

---
+++ src/PN7150.h
@@ -0,0 +1,2 @@
+// This file is here because the Arduino Library Checks likes to have a file with <libraryName>.h
+// As this library consists of 3 separate parts, each with their own .h file, this one can be empty
